52a84efd0afbb0eca2de41f8f49834a91f01c096
DevRayH <lewstherintalmon@gmail.com>
2025-01-23 04:50:12 -0600

    Add files via upload

---
+++ BST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Main{
+    class Program {
+        public static void Main(string[] args) {
+            int[] arr = { 3, 5, 2, 9, 6, 1 };
+            Tree tree = new Tree();
+
+            tree.FillTree(arr, 0);
+
+            tree.InOrder(tree.root);
+
+        }
+    }
+}
