944629dacdbdeaa3155d9cb61f48451162dc4eaf
DevRayH <lewstherintalmon@gmail.com>
2025-01-24 09:08:45 -0600

    Add files via upload

---
+++ Deque/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Main
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+            Deque dq = new Deque();
+            int[] myArray = { 4, 2, 3, 5, 8, 6, 7 };
+
+            Program.RecursiveFill(myArray, 0, dq);
+            Program.RecursiveEmpty(dq);
+        }
+
+        static void RecursiveFill(int[] list, int value, Deque deque)
+        {
+            if(value < list.Length)
+            {
+                deque.Push(list[value]);
+                RecursiveFill(list, value + 1, deque);
+            }
+        }
+
+        static void RecursiveEmpty(Deque deque){
+            if(deque.HasNodes == true)
+            {
+                WriteLine(deque.Pop().value);
+                RecursiveEmpty(deque);
+            }
+        }
+    }
+}
