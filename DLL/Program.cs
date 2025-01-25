00540eaa60afaa00c7e587c1306512c6f65d7aea
DevRayH <lewstherintalmon@gmail.com>
2025-01-25 08:34:50 -0600

    Add files via upload

---
+++ DLL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Main{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] values = {5, 3, 1, 9, 4};
+
+            LinkedList linkedList = new LinkedList();
+            FillList(ref values, ref linkedList);
+            linkedList.WalkList(linkedList);
+        }
+
+        static void FillList(ref int[] list, ref LinkedList passedList){
+            for (int iter = 0; iter < list.Length; iter++) { 
+                passedList.Insert(list[iter]);
+            }
+        }
+    }
+}
